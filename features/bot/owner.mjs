05870e4ecec3730fed12ad4ffb37d966cc8a8012
esteh1532 <130610158+esteh1532@users.noreply.github.com>
2023-05-16 01:06:47 +0700

    Update owner.mjs

--- features/bot/owner.mjs
+++ features/bot/owner.mjs
@@ -3,7 +3,7 @@ const handle = {
   category: "#bot",
   describe: "melihat contact developer",
   master: async (m, { q, conn }) => {
-    let kontak = [["Iqbal", q.developer[0], ""]];
+    let kontak = [["EsTeh Plastik", q.developer[0], ""]];
     await conn.sendkon(m.chat, q.name, kontak, m).catch((v) => conn.sendteks(m.chat, q.gagal, m));
   }
 };
